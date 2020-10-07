0abb9ddeab09828778003660c94ae22da988700d
luuislipee <luisfelipe.sbs@gmail.com>
2020-10-07 15:25:36 -0300

    Update alpine.sh

--- scripts/alpine.sh
+++ scripts/alpine.sh
@@ -1,4 +1,4 @@
 echo 'http://dl-cdn.alpinelinux.org/alpine/v3.6/main' >> /etc/apk/repositories
 echo 'http://dl-cdn.alpinelinux.org/alpine/v3.6/community' >> /etc/apk/repositories
 apk update
-apk add --no-cache mongodb=4.4.1-r0
+apk add --no-cache mongodb=4.4.1
